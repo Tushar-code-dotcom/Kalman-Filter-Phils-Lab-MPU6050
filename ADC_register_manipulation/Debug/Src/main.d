3341ac648b117c78ab1ffca1ff2a66a48d0b8cf9
Tushar-code-dotcom <vtushar5678@gmail.com>
2026-02-10 03:41:51 +0530

    ADC-bare-metal

---
+++ ADC_register_manipulation/Debug/Src/main.d
@@ -0,0 +1,18 @@
+Src/main.o: ../Src/main.c ../Inc/adc.h \
+ C:/Users/Tushar/STM32CubeIDE/workspace_1.16.0/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/Tushar/STM32CubeIDE/workspace_1.16.0/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h \
+ C:/Users/Tushar/STM32CubeIDE/workspace_1.16.0/Chip_headers/CMSIS/Include/core_cm4.h \
+ C:/Users/Tushar/STM32CubeIDE/workspace_1.16.0/Chip_headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/Tushar/STM32CubeIDE/workspace_1.16.0/Chip_headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/Tushar/STM32CubeIDE/workspace_1.16.0/Chip_headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Tushar/STM32CubeIDE/workspace_1.16.0/Chip_headers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/Tushar/STM32CubeIDE/workspace_1.16.0/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+../Inc/adc.h:
+C:/Users/Tushar/STM32CubeIDE/workspace_1.16.0/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/Tushar/STM32CubeIDE/workspace_1.16.0/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h:
+C:/Users/Tushar/STM32CubeIDE/workspace_1.16.0/Chip_headers/CMSIS/Include/core_cm4.h:
+C:/Users/Tushar/STM32CubeIDE/workspace_1.16.0/Chip_headers/CMSIS/Include/cmsis_version.h:
+C:/Users/Tushar/STM32CubeIDE/workspace_1.16.0/Chip_headers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/Tushar/STM32CubeIDE/workspace_1.16.0/Chip_headers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/Tushar/STM32CubeIDE/workspace_1.16.0/Chip_headers/CMSIS/Include/mpu_armv7.h:
+C:/Users/Tushar/STM32CubeIDE/workspace_1.16.0/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
